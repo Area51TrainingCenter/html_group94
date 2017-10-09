12e632ba640ec23322ef188c459f32ab02d4fff0
jtorriccelli <jcarlos@sminnova.com>
2017-10-09 14:13:37 -0500

    update clases

---
+++ modulo03/clase 01/main.js
@@ -0,0 +1,52 @@
+/*
+Sintaxis => la forma en la que se escriben las cosas en el LP
+palabras reservadas => palabras que el lenguaje tiene separadas para funciones especificas
+var , function , console , let 
+*/
+
+console.log("1era Clase de JS")
+
+console.log("esta es otra linea de texto en la consola")
+
+
+var dato1;
+var dato2;
+var dato3;
+var dato4;
+var dato5;
+var resultado;
+
+dato1="juan Carlos";
+dato2='Ramos Torriccelli';
+dato3="jueves";
+
+dato4=4;
+dato5=2;
+/*
+
++ suma 
+- resta
+/ division
+* multiplicacion
+% modular
+pow(10,4)
+sqrt(144)
+*/
+//resultado=dato4*dato5;
+var raiz;
+var potencia;
+var mayor;
+var menor;
+var aleatorio;
+raiz=Math.sqrt(144);
+potencia=Math.pow(2,3);
+mayor=Math.max(10,22,12,5,8);
+menor=Math.min(5,34,3,12,39);
+
+aleatorio=Math.random()*100;
+console.log("el numero aleatorio es:");
+console.log(aleatorio);
+console.log("el numero mayor es")
+console.log(mayor);
+console.log("el numero menor es:")
+console.log(menor);
